2090ab97761ab2d7f46d0f86c1402c29b10c91eb
Serafim <mr.d0c@yandex.ru>
2025-07-11 18:56:48 +0300

    Revert "скорректировать архитектуру, добавить UserService и FilmService v0.1"
    This reverts commit 1223ebdb

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage.film;
 import ru.yandex.practicum.filmorate.model.Film;
 import java.util.Collection;
 
-
 public interface FilmStorage {
     Film addFilm(Film film);
     Film updateFilm(Film film);
